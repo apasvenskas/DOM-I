ebb3c0e5940c9cd60caab4a3559a0f69b1632d6d
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-04-22 09:31:55 -0700

    work in progress2

--- src/index.js
+++ src/index.js
@@ -73,3 +73,11 @@ topContent.children[1].children[0].textContent = siteContent['main-content']['ab
 
 //bottom content
 const bottomContent = document.querySelector('.bottom-content')
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-content']
+h4sBottom[1].textContent = siteContent['main-content']['product-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+const psBottom = bottomContent.querySelectorAll('p')
+psBottom[0].textContent = siteContent['main-content']['services-content']
+psBottom[1].textContent = siteContent['main-content']['product-content']
+psBottom[2].textContent = siteContent['main-content']['vision-content']
